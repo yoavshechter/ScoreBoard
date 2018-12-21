87afba2ee8dbf72398fbc1864633c4cd30667392
Yoav Shechter <yoavshe6@gmail.com>
2018-12-22 00:14:15 +0200

    Revert "Adding I/O functions."
    This reverts commit 20e0ffb1f4458edb63f03ed66d60652a8d30c61a.

--- ScoreBoard/ScoreBoard/Simulator.c
+++
@@ -1,6 +0,0 @@
-#include "Simulator.h"
-
-int simulator(char * cfg, char * memin, char * memout, char * regout, char * traceinst, char * traceunit)
-{
-	return 0;
-}
--- ScoreBoard/ScoreBoard/Simulator.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <stdio.h>
-#include <stdlib.h>
-
-int simulator(char* cfg, char* memin, char* memout, char* regout, char* traceinst, char* traceunit);
